6707bf39c072974e245cc145d6ac72f65dff538b
PedroLucasLopes <pedrolopesparaguai@gmail.com>
2023-02-21 18:11:21 -0300

    Making Jest tests and adding JWT user auth

--- src/test/index.test.js
+++ src/test/index.test.js
@@ -1,7 +1,22 @@
 import request from 'supertest';
 import app from '../app.js';
 
-test('Test of CRUD requisitions', async () => {
-    const res = await request(app).get('/products');
-    expect(res.body).toHaveProperty('name', 'description', 'price');
+describe('Test of Products CRUD', () => {
+    test('Test of CRUD GET properties', async () => {
+        const res = await request(app).get('/products');
+        expect(res.body[0]).toHaveProperty('name', 'description', 'price');
+        expect(res.error).toBe(false);
+        expect(res.status).toBe(200);
+    })
+
+    test('Test of CRUD POST a product', async () => {
+        const product = {
+            name: 'Frigideira Jest',
+            description: 'Frigideira feita por teste automatizado',
+            price: 100.00
+        }
+        const res = await request(app).post('/products').send(product);
+        expect(res.error).toBe(false);
+        expect(res.status).toBe(201);
+    })
 })
